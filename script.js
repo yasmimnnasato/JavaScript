2335356f15a77b4bbf3e1ae9932f3f9618892460
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-02-06 21:21:19 -0300

    aula

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+function alterarTexto () {
+    let div = document.getElementById('minhaDiv');
+    let campo = document.getElementById('campoTexto');
+    div.innerHTML = '<p> O texto digitado pelo usuário é: '+campo.value+'</p>';
+}
 
+function validarResultado() {
+    let resultado = document.getElementById('resultado');
 
-function alertar () {
-    alert('Aproveite a nossa promoção e compre um curso nosso');
+    let resultadoConta = document.getElementById('resultadoConta');
+    const RESULTADOVALIDO = 4;
+    if (resultadoConta.value == RESULTADOVALIDO) {
+        resultado.innerHTML = '<p class="sucesso">Parabéns, você acertou! :D </p>';  }
+        else {
+            resultado.innerHTML = '<p class="erro">Resposta errada ): <br/> Tente novamente. </p>';
+        }
+}
+
+function contadorClicks () {
+    let numeroZero = document.getElementById('numeroZero');
+    let valor = parseInt (numeroZero.innerText) +1;
+
+    numeroZero.innerText = valor;
 }
